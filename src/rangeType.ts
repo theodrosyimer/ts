b88bda2d3fc3e6b8e83212b8158250addd91ec78
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-03 14:22:11 +0100

    style: refactor rangeType.ts to improve readability and maintainability

--- src/rangeType.ts
+++ src/rangeType.ts
@@ -5,11 +5,9 @@ type MAXIMUM_ALLOWED_BOUNDARY = 999
 type Mapped<
   N extends number,
   Result extends Array<unknown> = [],
-  > =
-  (Result['length'] extends N
-    ? Result
-    : Mapped<N, [...Result, Result['length']]>
-  )
+> = Result['length'] extends N
+  ? Result
+  : Mapped<N, [...Result, Result['length']]>
 
 type NumberRange = Mapped<MAXIMUM_ALLOWED_BOUNDARY>[number]
 
@@ -19,23 +17,40 @@ type Dictionary = {
 
 // from: [typescript - Is it possible to restrict number to a certain range - Stack Overflow](https://stackoverflow.com/questions/39494689/is-it-possible-to-restrict-number-to-a-certain-range)
 
-type Ran<T extends number> = number extends T ? number : _Range<T, []>;
-type _Range<T extends number, R extends unknown[]> = R['length'] extends T ? R[number] : _Range<T, [R['length'], ...R]>;
+type Ran<T extends number> = number extends T ? number : _Range<T, []>
+type _Range<T extends number, R extends unknown[]> = R['length'] extends T
+  ? R[number]
+  : _Range<T, [R['length'], ...R]>
 
 // from: [typescript - Is it possible to restrict number to a certain range - Stack Overflow](https://stackoverflow.com/questions/39494689/is-it-possible-to-restrict-number-to-a-certain-range)
 
-type PrependNextNum<A extends Array<unknown>> = A['length'] extends infer T ? ((t: T, ...a: A) => void) extends ((...x: infer X) => void) ? X : never : never;
+type PrependNextNum<A extends Array<unknown>> = A['length'] extends infer T
+  ? ((t: T, ...a: A) => void) extends (...x: infer X) => void
+    ? X
+    : never
+  : never
 
-type EnumerateInternal<A extends Array<unknown>, N extends number> = { 0: A, 1: EnumerateInternal<PrependNextNum<A>, N> }[N extends A['length'] ? 0 : 1];
+type EnumerateInternal<A extends Array<unknown>, N extends number> = {
+  0: A
+  1: EnumerateInternal<PrependNextNum<A>, N>
+}[N extends A['length'] ? 0 : 1]
 
-export type Enumerate<N extends number> = EnumerateInternal<[], N> extends (infer E)[] ? E : never;
+export type Enumerate<N extends number> = EnumerateInternal<
+  [],
+  N
+> extends (infer E)[]
+  ? E
+  : never
 
-export type Range<FROM extends number, TO extends number> = Exclude<Enumerate<TO>, Enumerate<FROM>>;
+export type Range<FROM extends number, TO extends number> = Exclude<
+  Enumerate<TO>,
+  Enumerate<FROM>
+>
 
-type E1 = Enumerate<43>;
+type E1 = Enumerate<43>
 
-type E2 = Enumerate<10>;
+type E2 = Enumerate<10>
 
-type R1 = Range<1, 5>;
+type R1 = Range<1, 5>
 
-type R2 = Range<0, 43>;
+type R2 = Range<0, 43>
