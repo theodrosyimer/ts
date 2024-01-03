6d14b0c526e0b7d1fa57c8defc2bb44f50679378
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-03 14:19:16 +0100

    refactor: refactor randomizer functions

--- src/randomizer.ts
+++ src/randomizer.ts
@@ -4,30 +4,41 @@ export type Randomizer =
   | ((max?: number) => number)
   | ((min?: number, max?: number) => number)
 
-export const random = (max?: number) =>
-  max != null ? Math.round(Math.random() * max) : Math.random()
+export function random(max?: number): number {
+  return max != null ? Math.round(Math.random() * max) : Math.random()
+}
 
-const random2 = (max?: number) =>
-  max != null ? Math.floor(Math.random() * (max + 1)) : Math.random()
+export function random2(max?: number): number {
+  return max != null ? Math.floor(Math.random() * (max + 1)) : Math.random()
+}
 
-export const randomMinMax = (min = 0, max = 0) =>
-  Math.max(Math.round(Math.random() * max), min)
+export function randomMinMax(min = 0, max = 0): number {
+  return Math.max(Math.round(Math.random() * max), min)
+}
 
-const randomMinMax2 = (min = 0, max = 0) =>
-  Math.floor(Math.random() * (max - min + 1)) + min
+export function randomMinMax2(min = 0, max = 0): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
 
-export const getRandomBooleanFromPercentage = (percentage: number) =>
-  Math.random() < percentage / 100
+export function getRandomBooleanFromPercentage(percentage: number): boolean {
+  return Math.random() < percentage / 100
+}
 
-export const randomBetween = <T, U>(first: T, second: U): T | U =>
-  randomMinMax(0, 1) === 0 ? first : second
+export function randomBetween<T, U>(first: T, second: U): T | U {
+  return randomMinMax(0, 1) === 0 ? first : second
+}
 
-export const headOrTails = () => randomBetween('heads', 'tails')
+export function headOrTails(): string {
+  return randomBetween('heads', 'tails')
+}
 
-export const maybeNullishOr = <T>(value: T) =>
-  randomBetween(value, randomBetween(null, undefined))
+export function maybeNullishOr<T>(value: T): T | null | undefined {
+  return randomBetween(value, randomBetween(null, undefined))
+}
 
-export const maybeTrueOrFalse = () => randomBetween(true, false)
+export function maybeTrueOrFalse(): boolean {
+  return randomBetween(true, false)
+}
 
 random() /*?. $*/
 random2() /*?. $*/
