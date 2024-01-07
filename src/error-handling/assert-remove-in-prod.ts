ca8d2e5ecc6868c9dda73e320d73ae1eb9cb3fa9
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-07 14:11:15 +0100

    feat: add assertInstanceOf function and remove unnecessary code

---
+++ src/error-handling/assert-remove-in-prod.ts
@@ -0,0 +1,23 @@
+function assertInstanceOf<T>(
+  value: any,
+  expectedClass: new () => T,
+): asserts value is T {
+  // this is the import part!
+  if (import.meta.env.PROD) return
+  if (!(value instanceof expectedClass)) {
+    throw new Error(
+      `Expected value to be a ${expectedClass.name}, but was ${value.constructor.name}`,
+    )
+  }
+}
+
+document.body.insertAdjacentHTML('beforeend', '<h1>Hello</h1>')
+
+const main = document.querySelector('h1')
+assertInstanceOf(main, HTMLHeadingElement)
+main.textContent += ' world!'
+
+// the code will be tree-shaken to:
+document.body.insertAdjacentHTML('beforeend', '<h1>Hello</h1>')
+const main = document.querySelector('h1')
+main.textContent += ' world!'
