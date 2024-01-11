8dd74606e048b28d509deff0cb872ee33f7b42be
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-11 17:20:36 +0100

    refactor: refactor pipe functions and add compose2 and pipe2 alternatives

--- src/pipe.ts
+++ src/pipe.ts
@@ -1,25 +1,37 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 
 // only handle synchronous functions
 const pipe =
-  <T extends (...args: any) => any>(...fns: T[]) =>
+  <T extends (...args: any) => unknown>(...fns: T[]) =>
   (param: unknown) =>
     fns.reduce((result, fn) => fn(result), param)
 
 // Handle asynchronous AND synchronous functions
 const pipeP =
-  <T extends (...args: any) => any>(...fns: T[]) =>
+  <T extends (...args: any) => unknown>(...fns: T[]) =>
   (param: Promise<unknown>) =>
     fns.reduce(
       (result, fn) => (result.then && result.then(fn)) || fn(result),
       param,
     )
 
-// Order of execution is reversed, same as composition, point-free style
-const pipeR =
-  <T extends (...args: any) => any>(...fns: T[]) =>
+// Order of execution is right to left
+const compose =
+  <T extends (...args: any) => unknown>(...fns: T[]) =>
   (param: unknown) =>
-    fns.reverse().reduce((result, fn) => fn(result), param)
+    fns.reduceRight((result, fn) => fn(result), param)
 
+function compose2<T extends(...args: any) => unknown>(...fns: T[]) {
+  return pipe(...fns.reverse())
+}
+function pipe2<T extends(...args: any) => unknown>(...fns: T[]) {
+  return (result: unknown) => {
+    for (const fn of fns) {
+      result = fn(result)
+    }
+    return result
+  }
+}
 export {}
