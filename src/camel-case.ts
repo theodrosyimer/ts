3516b32df09d5d406bf02d5c240884bb95837f7e
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-20 21:20:22 +0100

    refactor: refactor camelCase function and add new toCamelCase2 function

--- src/camel-case.ts
+++ src/camel-case.ts
@@ -7,24 +7,35 @@ const capitalizeArrayOfWords = (arr: string[]) =>
   arr
     .map(
       (x: string) =>
-        `${x.substring(0, 1).toUpperCase()}${x.substring(1).toLowerCase()}`
+        `${x.substring(0, 1).toUpperCase()}${x.substring(1).toLowerCase()}`,
     )
     .join('')
 
 export const toCamelCase = (string: string): string | Error => {
-  const _string = string
-
-  if (_string == null || typeof _string !== 'string')
+  if (typeof string !== 'string') {
     return new Error(`Expected: an input of type 'string', Received: ${string}`)
+  }
 
-  const arrayOfWords = _string.replace(/\n | \t/, ' ').split(' ')
-  if (arrayOfWords && arrayOfWords.length === 1) return _string.toLowerCase()
+  const arrayOfWords = string
+    .slice()
+    .replace(/\n | \t/, ' ')
+    .split(' ')
+  if (arrayOfWords.length === 1) return string.toLowerCase()
 
   const [first, ...rest] = arrayOfWords
   return `${first?.toLowerCase()}${capitalizeArrayOfWords(rest)}`
 }
 
-const [ex0, ex1, ex2, ex3, ex4, ex5, ex6, ex7] = [
+export function toCamelCase2(str: string) {
+  if (str.includes('_')) {
+    const [start, ...rest] = str.split('_')
+    return `${start}${capitalizeArrayOfWords(rest)}`
+  }
+  return str
+}
+toCamelCase2('first_name_test') /*? .$*/
+
+const [ex0, ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8] = [
   'first_name',
   'MulTiple   spaCEs  tEst',
   'newline \n tEst',
@@ -59,6 +70,7 @@ toCamelCase(ex4) /*? .$*/
 toCamelCase(ex5) /*? .$*/
 toCamelCase(ex6) /*? .$*/
 toCamelCase(ex7) /*? .$*/
+toCamelCase(ex8) /*? .$*/
 
 // toCamelCase(`[
 //   {
