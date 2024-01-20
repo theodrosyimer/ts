df6bb01d57752d71fa180db1d03e9f9ed813bec6
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-20 21:18:31 +0100

    feat: add error instance validation for integer conversion

--- src/error-handling/error-instance-validation.ts
+++ src/error-handling/error-instance-validation.ts
@@ -1,10 +1,15 @@
+/* eslint-disable max-classes-per-file */
 // source: [How to declare a function that throws an error in Typescript - Stack Overflow](https://stackoverflow.com/a/74012773)
 class ValidationError extends Error {
   constructor(readonly description: string) {
     super(description)
-
+    // Set the prototype explicitly only needed if i need to support:
+    //  - node < 6
+    //  - IE < 11
+    //  - ES < 2015
     Object.setPrototypeOf(this, new.target.prototype)
 
+    // ! don't need it?
     Error.captureStackTrace(this)
   }
 
@@ -20,13 +25,12 @@ class ValidationError extends Error {
 
 function toInt(num: string): number | ValidationError {
   const result = Number.parseInt(num)
-  if (result == null || Number.isNaN(result)) {
-    return new ValidationError(`Invalid integer ${num}`)
+  if (Number.isNaN(result)) {
+    throw new ValidationError(`Invalid integer ${num}`)
   }
   return result
 }
 
-// caller
 const result = toInt('a')
 
 if (ValidationError.isInstance(result)) {
