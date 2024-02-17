3dbcc946f7dee4ef89c189963e1938aad8370a3a
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-17 19:56:38 +0100

    Fix error handling in getCallerLocation function

--- src/error-handling/error-get-caller-location.ts
+++ src/error-handling/error-get-caller-location.ts
@@ -1,13 +1,18 @@
 // inspiration: [Understanding JavaScript Error Stack Traces (Call Frames) for Capturing Decorator Error Locations | by Moxi | Medium](https://medium.com/@fwx5618177/understanding-javascript-error-stack-traces-call-frames-for-capturing-decorator-error-locations-6e8ea61e3c72)
 export function getCallerLocation() {
-  const error = {}
+  const error = new Error()
   Error.captureStackTrace(error, getCallerLocation)
 
-  const stackLines = error.stack.split('\n')
+  const stackLines = error.stack?.split('\n')
+
+  if (!stackLines) {
+    return 'Unknown Location'
+  }
 
   // Assuming the call location is on the third line of the stack
   if (stackLines.length >= 4) {
-    return stackLines[2].trim()
+    return stackLines[2]?.trim()
   }
-  return 'Unknown Location'
+
+  return stackLines[0]?.trim()
 }
