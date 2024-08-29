193222371b9f4435122368e48cef7b7313847b83
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-08-29 06:23:49 +0200

    refactor: Update queryBuilder function to use Methods type instead of WhereOptions

--- src/sql-like-api.ts
+++ src/sql-like-api.ts
@@ -1,15 +1,13 @@
 /* eslint-disable no-empty-pattern */
-type Methods = {
-  eq: (value: number) => boolean
-}
+type Methods = typeof methods
 
-function queryBuilder() {
-  const methods: Methods = {}
-  const id = 1
-  function eq(value: number) {
-    return id === value
-  }
+const methods = {
+  eq(value1: unknown, value2: unknown) {
+    return value1 === value2
+  },
+} as const
 
+function queryBuilder<const T extends Record<string, unknown>>() {
   return {
     where(cb: (methods: Methods) => void) {
       return cb(methods)
@@ -17,4 +15,10 @@ function queryBuilder() {
   }
 }
 
-queryBuilder().where(({}) => ({}))
+function buildQueryBuilder<const T extends Record<string, unknown>>() {
+  return queryBuilder<T>()
+}
+
+const myQueryBuilder = buildQueryBuilder()
+
+myQueryBuilder.where(({}) => ({}))
