3b25ceb4811f0e7916b1bfcd071a4e33b3953004
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-09-06 17:54:50 +0200

    refactor: Update queryBuilder function to use Methods type instead of WhereOptions

--- src/sql-like-api.ts
+++ src/sql-like-api.ts
@@ -1,16 +1,64 @@
 /* eslint-disable no-empty-pattern */
-type Methods = typeof methods
+
+const user = {
+  id: 1,
+  name: 'Theo',
+  email: 'example@example.com',
+  password: 'mysecret',
+  role: 'admin',
+}
+
+type User = typeof user
 
 const methods = {
-  eq(value1: unknown, value2: unknown) {
+  eq<T>(value1: T, value2: T) {
     return value1 === value2
   },
 } as const
 
+type Methods = typeof methods
+
+type Callback = (methods: Methods) => void
+
+/**
+ * Implement drizzle's client API
+ *
+ * @example
+ * ```ts
+ * db.select().from(user)
+ *
+ * db
+      .select()
+      .from(user)
+      .limit(limit)
+      .offset((page - 1) * limit)
+ *
+ * db
+      .select({ id: user.id, email: user.email, password: user.password })
+      .from(user)
+      .where(eq(user.email, email))
+ *
+ * db.insert(user).values(newUser)
+ *
+ * db.query.movie.findMany({})
+ * db.query.user.findFirst({
+      where: (user, { eq }) => eq(user.id, id),
+      columns: {
+        password: false,
+      },
+    })
+ * ```
+ */
 function queryBuilder<const T extends Record<string, unknown>>() {
   return {
-    where(cb: (methods: Methods) => void) {
-      return cb(methods)
+    select(cb: Callback) {
+      cb(methods)
+      return {
+        where(/* user: T,  */ cb: Callback) {
+          cb(methods)
+          return this
+        },
+      }
     },
   }
 }
@@ -19,6 +67,6 @@ function buildQueryBuilder<const T extends Record<string, unknown>>() {
   return queryBuilder<T>()
 }
 
-const myQueryBuilder = buildQueryBuilder()
+const myQueryBuilder = buildQueryBuilder<User>()
 
-myQueryBuilder.where(({}) => ({}))
+myQueryBuilder.select(({ eq }) => eq(user.id, 1)).where(() => ({}))
