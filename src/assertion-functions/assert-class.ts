0c0f1c6c5c6e2a7a8251768f7d5b9fd1016cf619
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2026-03-02 12:46:08 +0100

    refactor: restructure SDK class

--- src/assertion-functions/assert-class.ts
+++ src/assertion-functions/assert-class.ts
@@ -1,20 +1,30 @@
 // source: [Use assertion functions inside classes | Total TypeScript](https://www.totaltypescript.com/tips/use-assertion-functions-inside-classes)
 export class SDK {
-  constructor(public loggedInUserId?: string) {}
+  #repository: Repository
+  loggedInUserId?: string | undefined
+
+  constructor(loggedInUserId?: string) {
+    this.#repository = new Repository()
+    this.loggedInUserId = loggedInUserId
+  }
 
   createPost(title: string) {
     this.assertUserIsLoggedIn()
 
-    createPost(this.loggedInUserId, title)
-    this.wow
+    this.#repository.createPost(this.loggedInUserId, title)
   }
 
   assertUserIsLoggedIn(): asserts this is this & {
     loggedInUserId: string
-    wow: boolean
   } {
     if (this.loggedInUserId) {
       throw new Error('User is not logged in')
     }
+  }
+}
+
+class Repository {
+  createPost(userId: string, title: string) {
+    console.log(`Creating post ${title} for user ${userId}`)
   }
 }
