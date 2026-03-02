d33bb81d00ae30c4054a9075b5ed84e8627b964c
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2026-03-02 12:59:01 +0100

    feat: add inferReadOnlyObject and inferKey functions

--- src/generics/object-inferred-key-value-as-const.ts
+++ src/generics/object-inferred-key-value-as-const.ts
@@ -4,6 +4,21 @@ function inferObject<T extends Record<Key, V>, V extends string>(obj: T) {
   return obj
 }
 
+function inferReadOnlyObject<const T extends Record<any, any>>(obj: T) {
+  return obj
+}
+
 const oTyped = inferObject({ apikey: '0123456789', name: 'John Doe' })
+const oTyped2 = inferReadOnlyObject({ apikey: '0123456789', name: 'John Doe' })
+
+function inferKey<T extends Record<any, any>, K extends keyof T>(
+  obj: T,
+  key: K,
+) {
+  return obj[key]
+}
+
+// only return the type and not the literal value
+const oTyped3 = inferKey(o, 'name')
 
 export {}
